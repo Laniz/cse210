77a8b44fdcbcfed0883e7a56c3f576e18b70e604
laniz <shdrocks.ncube@gmail.com>
2024-03-18 00:27:04 -0600

    aaa

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -263,7 +263,7 @@ class Program
     {
         Console.WriteLine("Select an option:");
         Console.WriteLine("1. Create New goal");
-        Console.WriteLine("2. List Goals (not yet saved)");
+        Console.WriteLine("2. List Goals");
         Console.WriteLine("3. Save Goals");
         Console.WriteLine("4. Load Goals");
         Console.WriteLine("5. Record Event");
